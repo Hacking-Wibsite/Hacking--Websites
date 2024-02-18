104eb3c01e2286ca05917d1543bf5d0f01ba310a
Hacking-Wibsite <160402395+Hacking-Wibsite@users.noreply.github.com>
2024-02-19 04:23:05 +1300

    Add files via upload

---
+++ brut force.py
@@ -0,0 +1,38 @@
+import requests
+
+URL = input("Eenter Your Target :")
+
+File = input("Eenter Your File :")
+
+replace = URL.replace(URL,"https://")
+
+try:
+    
+    x = open(File)
+    
+    for i in x :
+        
+        r = requests.get(replace + URL + i)
+        
+        v = replace + URL + i
+        
+        #print(r.status_code)
+        
+    if r.status_code == 200 :
+        
+        print("Conterol panle",r.status_code,":",v)
+        
+    if r.status_code == 400 :
+        
+        print("x Error",r.status_code,":",v)
+        
+    if r.status_code == 404 :
+        
+        print("x Error",r.status_code,":",v)
+        
+except:
+    
+    print("Internet Error")
+
+
+    
